028c9f60a625b4366eb4813ee97568e26aa13507
Rodrigo <rodrigovf2102@gmail.com>
2022-09-28 12:09:22 -0300

    feat: project initialized

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+const port = process.env.PORT;
+
+server.listen(port);
