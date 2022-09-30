eef7c2b7569b28d58c53927ebceb2b500deeb20e
Rodrigo <rodrigovf2102@gmail.com>
2022-09-30 11:25:09 -0300

    feat: JOIN tables games with categories implemented on getGames

--- src/controllers/Games.controllers.js
+++ src/controllers/Games.controllers.js
@@ -2,14 +2,23 @@ import { StatusCodes } from "http-status-codes";
 import connection from "../database.js";
 
 async function getGames(req, res) {
-    const {name} = req.query;
+    const { name } = req.query;
+    let nameCaseInsensitive = '';
+    if (name) {
+        for (let i = 0; i < name.length; i++) {
+            if (i === 0) nameCaseInsensitive += name[i].toUpperCase();
+            if (i !== 0) nameCaseInsensitive += name[i].toLowerCase();
+        }
+    }
     try {
         let games;
-        if(name){
-            games = await connection.query('SELECT * FROM games WHERE name LIKE $1',[`${name}%`]);
+        if (name) {
+            games = await connection.query(
+                'SELECT games.*,categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id WHERE games.name LIKE $1', [`${nameCaseInsensitive}%`]);
         }
-        if(!name){
-            games = await connection.query('SELECT * FROM games');
+        if (!name) {
+            games = await connection.query(
+                'SELECT games.*,categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id');
         }
         if (games.rows.length < 1) {
             return res.status(StatusCodes.NOT_FOUND).send('Games table is empty');
