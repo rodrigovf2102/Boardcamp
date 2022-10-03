5f28d09cb348c2affeeacc0107b56755802c8236
Rodrigo <rodrigovf2102@gmail.com>
2022-10-03 16:41:47 -0300

    feat: games pagination implemented

--- src/controllers/Games.controllers.js
+++ src/controllers/Games.controllers.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import connection from "../database.js";
 
 async function getGames(req, res) {
-    const { name } = req.query;
+    const { name, offset, limit } = req.query;
     let nameCaseInsensitive = '';
     if (name) {
         for (let i = 0; i < name.length; i++) {
@@ -12,16 +12,47 @@ async function getGames(req, res) {
     }
     try {
         let games;
-        if (name) {
-            games = await connection.query(
-                `SELECT games.*,categories.name AS "categoryName" 
-                FROM games JOIN categories ON games."categoryId"=categories.id 
-                WHERE games.name LIKE $1;`, [`${nameCaseInsensitive}%`]);
+        const standardSearch = `SELECT games.*,categories.name AS "categoryName" 
+        FROM games JOIN categories ON games."categoryId"=categories.id`;
+        if(!offset && !limit){
+            if (name) {
+                games = await connection.query(
+                    `${standardSearch} WHERE games.name LIKE $1;`, [`${nameCaseInsensitive}%`]);
+            }
+            if (!name) {
+                games = await connection.query(`${standardSearch};`);
+            }
         }
-        if (!name) {
-            games = await connection.query(
-                `SELECT games.*,categories.name AS "categoryName" 
-                FROM games JOIN categories ON games."categoryId"=categories.id;`);
+        if(offset && !limit){
+            if (name) {
+                games = await connection.query(
+                    `${standardSearch} WHERE games.name LIKE $1 OFFSET $2;`,
+                    [`${nameCaseInsensitive}%`,offset]);
+            }
+            if (!name) {
+                games = await connection.query(`${standardSearch} OFFSET $1;`,[offset]);
+            }
+        }
+        if(!offset && limit){
+            if (name) {
+                games = await connection.query(
+                    `${standardSearch} WHERE games.name LIKE $1 LIMIT $2;`,
+                    [`${nameCaseInsensitive}%`,limit]);
+            }
+            if (!name) {
+                games = await connection.query(`${standardSearch} LIMIT $1;`,[limit]);
+            }
+        }
+        if(offset && limit){
+            if (name) {
+                games = await connection.query(
+                    `${standardSearch} WHERE games.name LIKE $1 OFFSET $2 LIMIT $3;`,
+                    [`${nameCaseInsensitive}%`,offset,limit]);
+            }
+            if (!name) {
+                games = await connection.query(`${standardSearch} OFFSET $1 LIMIT $2;`,
+                [offset,limit]);
+            }
         }
         if (games.rows.length < 1) {
             return res.status(StatusCodes.NOT_FOUND).send('Games not found');
@@ -37,27 +68,27 @@ async function postGame(req, res) {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
         const category = (await connection
-            .query('SELECT * FROM categories WHERE id=$1;',[categoryId])).rows[0];
+            .query('SELECT * FROM categories WHERE id=$1;', [categoryId])).rows[0];
         const gameName = (await connection
-            .query('SELECT * FROM games WHERE name=$1;',[name])).rows[0];
-        if(category && !gameName){
+            .query('SELECT * FROM games WHERE name=$1;', [name])).rows[0];
+        if (category && !gameName) {
             await connection.query(
                 `INSERT INTO games (name,image,"stockTotal","categoryId","pricePerDay") 
                 VALUES ($1,$2,$3,$4,$5);`
                 , [name, image, stockTotal, categoryId, pricePerDay]);
             return res.sendStatus(StatusCodes.CREATED);
         }
-        if(!category && gameName){
+        if (!category && gameName) {
             return res.status(StatusCodes.CONFLICT).
-            send('Error: category Id doesn`t exist and game name already exist');
+                send('Error: category Id doesn`t exist and game name already exist');
         }
-        if(!category){
+        if (!category) {
             return res.status(StatusCodes.BAD_REQUEST).send('Error: category Id doesn`t exist');
         }
-        if(gameName){
+        if (gameName) {
             return res.status(StatusCodes.CONFLICT).send('Error: game name already exist');
         }
-    } catch(error){
+    } catch (error) {
         console.log(error);
         return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     }
